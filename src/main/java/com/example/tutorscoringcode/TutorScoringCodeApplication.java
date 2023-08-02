2fceb2f31b09e901d9b8d9665106518ed7268332
Allegra Anka-Mueller <allegraanka-mueller@Allegras-MacBook-Pro.local>
2023-08-02 11:17:17 +0100

    First commit

---
+++ src/main/java/com/example/tutorscoringcode/TutorScoringCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorscoringcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorScoringCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorScoringCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tutorscoringcode/TutorScoringCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorscoringcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorScoringCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
